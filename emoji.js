2cdb9d023637cc8cbc2b0ab6f238bdd8685ec9c2
EagleNerd <ugorjimiracle06@gmail.com>
2020-11-26 15:38:05 +0100

    images resized according to the client window size

--- emoji.js
+++ emoji.js
@@ -1,10 +1,23 @@
-alert("Hello there! I am your pet emoji");
-
+var userName;
 function emojiGreets(){
-	let name = prompt("What is your name? Type your name here..." );
-	if(name){
-		alert("Nice to meet you, " +  name + ".");
-		document.getElementById('emoji').src = 'images/smiley-emoji.jpg';
-		setInterval("document.getElementById('emoji').src = 'images/waving-emoji.jpg';", 6000);
+alert("Hello there! I am your pet emoji");
+}
+function touchEmoji(){
+	if (userName){
+		alert("I like the attention, " + userName +". Thank you.")
+
+	} else {
+
+		userName = prompt("What is your name?", "Enter your name here.");
+
+	if (userName){
+		alert("It's my pleasure to meet you, " + userName + ".");
 	}
+		document.getElementById('emoji').src = 'images/smiley-emoji.jpg';
+		setTimeout("document.getElementById('emoji').src = 'images/waving-emoji.jpg';", 6000);
+	}
+	
+}
+function resizeEmoji(){
+	document.getElementById("emoji").style.height = (document.body.clientHeight-100) * 0.7; /* subtract 100px to account for the vertical position of the image, multiplied by 70% of the remaining window size. */
 }
